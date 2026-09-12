b2cf1f82626608c72a36d247e5f26d531298a55a
Darshan801 <darshankunwar79@gmail.com>
2026-09-12 23:29:32 +0545

    Initial frontend setup with React + Vite

---
+++ frontend/src/components/layout/AnnouncementBar.jsx
@@ -0,0 +1,16 @@
+// at first top annuncement part 
+
+
+
+// LuminAID site has promotional/announcement content at the top
+// so we will make reusable layout Component
+
+import React from 'react'
+
+const AnnouncementBar = () => {
+  return (
+    <div>AnnouncementBar</div>
+  )
+}
+
+export default AnnouncementBar
---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,10 @@
+// header here
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
